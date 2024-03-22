c3b53c17d1ef3bd574b5f80d9eff1e5e19bd8423
iamdoublea <aadityaseal@gmail.com>
2024-03-22 16:05:47 +0530

    component architecture

---
+++ src/components/model_evaluation.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from src.logger.logging import logging
+from src.exception.exception import customexception
+
+
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path # we use to create a system independent path
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import pickle
+import src.DimondPricePrediction.utils.utils import load_object
+
+
+
+
+@dataclass
+class ModelEvaluationConfig:
+    pass
+
+
+class ModelEvaluation:
+    def __init__(self):
+        pass
+
+    def initiate_model_evaluation(self):
+        try:
+            pass
+        except Exception as e:
+            logging.info()
+            raise customexception(e,sys)
+        
+
+
